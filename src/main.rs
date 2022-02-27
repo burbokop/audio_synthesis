2736c93fb83fb3b6834bf6f70c768f01b457bf98
Your Name <burbokop@example.com>
2022-02-27 15:00:29 +0200

    +

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,18 @@
 mod source;
 mod sample;
+mod sink;
 
-use std::fs::File;
-use std::io::BufReader;
-use std::time::Duration;
-use rodio::cpal::Sample;
-use rodio::{Decoder, OutputStream};
-use rodio::source::{SineWave, Source};
+use rodio::OutputStream;
 
-
-
-
-
-
-
-
-use fon::chan::{Ch16, Channel};
-use fon::{Audio, Frame, Sink};
-use sample::ChanSampleFormat;
+use fon::chan::Ch16;
+use fon::Frame;
 use twang::noise::White;
 use twang::ops::Gain;
 use twang::osc::Sine;
 use twang::Synth;
 
-use crate::source::MonoFrameSource;
+use crate::sink::PlaybackMonoSink;
+
 
 /// First ten harmonic volumes of a piano sample (sounds like electric piano).
 const HARMONICS: [f32; 10] = [
@@ -41,43 +30,6 @@ struct Processors {
     white: White,
     // 10 harmonics for 3 pitches.
     piano: [[Sine; 10]; 3],
-}
-
-struct MySink<'a> {
-    stream: &'a rodio::OutputStreamHandle
-}
-
-impl<'a> MySink<'a> {
-    fn new(stream: &'a rodio::OutputStreamHandle) -> Self { MySink { stream: stream } }
-}
-impl std::fmt::Debug for MySink<'_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        unimplemented!()
-    }
-}
-
-
-impl<'a, Chan: Channel + ChanSampleFormat> Sink<Chan, 1> for MySink<'a> {
-    fn sample_rate(&self) -> std::num::NonZeroU32 { std::num::NonZeroU32::new(48_000).unwrap() }
-
-    fn len(&self) -> usize { 48_000 * 10 }
-
-    fn sink_with(&mut self, iter: &mut dyn Iterator<Item = Frame<Chan, 1>>) {
-        println!("sink_with0");
-
-        let sink = rodio::Sink::try_new(self.stream).unwrap();
-
-
-        let mf = MonoFrameSource::new(iter, 48_000 * 10).collect_clone();
-        println!("sink_with2");
-
-        sink.append(mf);
-
-        sink.sleep_until_end();
-
-        //self.stream.play_raw(mf).unwrap();
-        println!("sink_with3");
-    }
 }
 
 
@@ -116,7 +68,7 @@ fn main() {
     // The sound plays in a separate thread. This call will block the current thread until the sink
     // has finished playing all its queued sounds.
 
-    let sink = MySink::new(&stream_handle);
+    let sink = PlaybackMonoSink::new(&stream_handle);
     //let mut audio = Audio::<Ch16, 2>::with_silence(48_000, 48_000 * 5);
    
 
---
+++ src/sink.rs
@@ -0,0 +1,32 @@
+use fon::{chan::Channel, Sink, Frame};
+
+use crate::{sample::ChanSampleFormat, source::MonoFrameSource};
+
+pub struct PlaybackMonoSink<'a> {
+    stream: &'a rodio::OutputStreamHandle
+}
+
+impl<'a> PlaybackMonoSink<'a> {
+    pub fn new(stream: &'a rodio::OutputStreamHandle) -> Self { PlaybackMonoSink { stream: stream } }
+}
+impl std::fmt::Debug for PlaybackMonoSink<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        unimplemented!()
+    }
+}
+
+
+impl<'a, Chan: Channel + ChanSampleFormat> Sink<Chan, 1> for PlaybackMonoSink<'a> {
+    fn sample_rate(&self) -> std::num::NonZeroU32 { std::num::NonZeroU32::new(48_000).unwrap() }
+
+    fn len(&self) -> usize { 48_000 * 10 }
+
+    fn sink_with(&mut self, iter: &mut dyn Iterator<Item = Frame<Chan, 1>>) {
+        let sink = rodio::Sink::try_new(self.stream).unwrap();
+        let source = MonoFrameSource::new(iter, Sink::<Chan, 1>::len(self)).collect_clone();
+        sink.append(source);
+        sink.sleep_until_end();
+        //self.stream.play_raw(mf).unwrap();
+    }
+}
+
